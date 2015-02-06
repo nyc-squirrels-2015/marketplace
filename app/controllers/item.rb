b780c52cd3f2086643886006134bfba7c238265c
ivanyukio <ivan.yk14@gmail.com>
2015-02-06 17:00:25 -0500

    add show and edit item without user verification

--- app/controllers/item.rb
+++ app/controllers/item.rb
@@ -1,3 +1,9 @@
+get "/categories/:category_id/items/:id" do
+  @item_to_display = Item.find(params[:id])
+
+  erb :"items/show_item"
+end
+
 get "/categories/:category_id/items/new" do
   erb :"items/new_item"
 end
@@ -8,3 +14,11 @@ post "/categories/:category_id/items/new" do
   redirect "/categories/#{params[:category_id]}"
 end
 
+get "/categories/:category_id/items/:id/update" do
+  @item_to_edit = Item.find(params[:id])
+  erb :"items/edit_item"
+end
+
+put "/categories/:category_id/items/:id/update" do
+  Item.update(params[:id], {:name => params[:name], :description => params[:description], price: params[:price]})
+end
